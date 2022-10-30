b7597331fb72237a00468558864a2ebdcac98fa9
Rhys Mason-Gorrie <74096576+rhysjamesmason@users.noreply.github.com>
2022-10-30 09:53:35 +0000

    RESET

--- components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import {Loading} from "@nextui-org/react";
-
-const LoadingComponent = ({imageURL, title, action, linkURL, cred}) => (
-  <center>
-    <Loading type="points" />
-  </center>
-);
-
-export default LoadingComponent
